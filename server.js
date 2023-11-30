f8c65a8f709a01d1cd034b97c72fd8ff5db27239
Marcel Pikul <marcelpikul2@gmail.com>
2023-11-30 16:41:31 +0100

    upd

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 //middleware
 app.use(bodyParser.json())
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://foodshare-0311.netlify.app/'
 }))
 
 //routes
